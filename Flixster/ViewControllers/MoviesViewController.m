7f6aa5d8288384ebd98cb1e48b6c088a7826ccdf
Tarini Singh <tarsingh@fb.com>
2018-06-28 16:07:59 -0700

    added pull to refresh functionality

--- Flixster/ViewControllers/MoviesViewController.m
+++ Flixster/ViewControllers/MoviesViewController.m
@@ -13,6 +13,7 @@
 @interface MoviesViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 @end
 
 @implementation MoviesViewController
@@ -23,6 +24,15 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+    
+}
+
+- (void)fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -47,6 +57,7 @@
             // Reload your table view data
             [self.tableView reloadData];
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
 }
